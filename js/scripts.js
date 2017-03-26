f20a440c3e0e4f9cc4c98236738e36f786ecf863
Sonia Rajs <sonia.ryszytylo@gmail.com>
2017-03-26 13:32:40 +0200

    triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 // plik scripts.js
-var name = prompt('Wpisz swoje imię');
+//var name = prompt('Wpisz swoje imię');
 //alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+//console.log('Witaj, ' + name);
 /*
 	inne komentarze
 */
 // lalala
+
+var a = prompt('wpisz wartosc a');
+var h = prompt('wpisz wwartość h');
+var triangleArea = a*h/2;
+alert('Twój wynik to, ' + triangleArea);
+console.log('pole trojkata');
